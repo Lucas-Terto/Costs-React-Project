a44d3147cd1d59de681c4dc3c1b1f780710f8826
Lucas-Terto <lucas47bk@gmail.com>
2022-07-20 20:43:37 -0300

    new project page v1

--- src/components/pages/NewProject.jsx
+++ src/components/pages/NewProject.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import ProjectForm from '../project/ProjectForm'
+
+import styles from './NewProject.module.css'
 
 const NewProject = () => {
   return (
-    <div>NewProject</div>
+    <div className={styles.newproject_container}>
+      <h1>Criar projeto</h1>
+      <p>Crie seu projeto para depois adicionar os serviços.</p>
+      <ProjectForm />
+    </div>
   )
 }
 
