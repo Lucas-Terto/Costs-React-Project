e643101145d1b792e5ac39aad8385b49f4c308c5
Lucas-Terto <lucas47bk@gmail.com>
2022-07-22 17:54:05 -0300

    messages-of-sstem

--- src/components/pages/NewProject.jsx
+++ src/components/pages/NewProject.jsx
@@ -24,7 +24,7 @@ const NewProject = () => {
       .then((data) => {
         console.log(data)
         //redirect
-        navigate('/projects', {message: 'Projeto criado com sucesso!'})
+        navigate('/projects', {state: {message: 'Projeto criado com sucesso!'}})
       })
       .catch((error) => console.log(error))
 
