16610b685e63a7f83b304c07c3ff66fcc3d91e8e
Lucas-Terto <lucas47bk@gmail.com>
2022-07-25 17:27:30 -0300

    services area

--- src/components/pages/Project.jsx
+++ src/components/pages/Project.jsx
@@ -15,6 +15,7 @@ const Project = () => {
     const [showProjectForm, setShowProjectForm] = useState(false)
     const [message, setMessage] = useState()
     const [type, setType] = useState()
+    const [showServiceForm, setShowServiceForm] = useState(false)
 
     useEffect(() => {
         setTimeout(() => {
@@ -32,6 +33,8 @@ const Project = () => {
     }, [id])
 
     function editPost(project) {
+        setMessage('')
+
         //budget validation
         if(project.budget < project.cost) {
             setMessage('O orçamento não pode ser menor que o custo do projeto!')
@@ -62,6 +65,10 @@ const Project = () => {
 
     function toggleProjectForm() {
         setShowProjectForm(!showProjectForm)
+    }
+
+    function toggleServiceForm() {
+        setShowServiceForm(!showServiceForm)
     }
 
   return (
@@ -97,6 +104,19 @@ const Project = () => {
                             </div>
                         )}
                     </div>
+                    <div className={styles.service_form_container}>
+                            <h2>Adicione um serviço</h2>
+                            <button onClick={toggleServiceForm} className={styles.btn}>
+                            {!showServiceForm ? "Adicionar serviço" : "Fechar"}
+                            </button>
+                            <div className={styles.project_info}>
+                                {showServiceForm && (<div>formulário do serviço</div>)}
+                            </div>
+                    </div>
+                    <h2>Serviços</h2>
+                    <Container customClass="start">
+                            <p>Itens de serviços</p>
+                    </Container>
                 </Container>
             </div>
         ) : (
